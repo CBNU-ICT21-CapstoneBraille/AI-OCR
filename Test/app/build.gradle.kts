ce67a58bb0df75ec633882de1f3056182e2ef4a6
ARONGEEE <146042899+ARONGEEE@users.noreply.github.com>
2024-08-16 16:39:06 +0900

    Update build.gradle.kts
    mlkit version update + korean

--- Test/app/build.gradle.kts
+++ Test/app/build.gradle.kts
@@ -32,22 +32,28 @@ android {
 }
 
 dependencies {
+    dependencies {
+        implementation("androidx.appcompat:appcompat:1.6.1")
+        implementation("com.google.android.material:material:1.9.0")
+        implementation("androidx.activity:activity:1.7.2")
+        implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+        testImplementation("junit:junit:4.13.2")
+        androidTestImplementation("androidx.test.ext:junit:1.1.5")
+        androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+        // CameraX Libraries
+        implementation("androidx.camera:camera-core:1.4.0-rc01")
+        implementation("androidx.camera:camera-camera2:1.4.0-rc01")
+        implementation("androidx.camera:camera-lifecycle:1.4.0-rc01")
+        implementation("androidx.camera:camera-video:1.4.0-rc01")
+        implementation("androidx.camera:camera-view:1.4.0-rc01")
+        implementation("androidx.camera:camera-mlkit-vision:1.4.0-rc01")
+        implementation("androidx.camera:camera-extensions:1.4.0-rc01")
 
-    //CameraX Libraries
-    val camerax_version = "1.4.0-rc01"
-    implementation(libs.androidx.camera.core)
-    implementation(libs.androidx.camera.camera2)
-    implementation(libs.camera.lifecycle)
-    implementation(libs.androidx.camera.video)
-    implementation(libs.androidx.camera.view)
-    implementation(libs.androidx.camera.mlkit.vision)
-    implementation(libs.androidx.camera.extensions)
+        // ML Kit 라이브러리 추가
+        implementation("com.google.mlkit:text-recognition:16.0.1")
+        implementation ("com.google.mlkit:text-recognition-korean:16.0.1")
+        implementation("com.google.mlkit:vision:16.3.0")
+    }
+
 }
